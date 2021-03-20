55d19407240aaf4cf3146d78d6747ec4d84e01bf
Denissant <denissanturyan@gmail.com>
2021-03-20 18:50:03 +0400

    Added  Heroku hyperlink

--- resource.py
+++ resource.py
@@ -13,4 +13,6 @@ def get_users(count):
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Welcome</h1> <br> Visit <code>https://uni-challenge-randuser.herokuapp.com/randusers/(count) </code> to receive random user data of the specified count. Example for 5 users: https://uni-challenge-randuser.herokuapp.com/randusers/5"
+    return """<h1>Welcome</h1> <br> Visit <code>https://uni-challenge-randuser.herokuapp.com/randusers/(count) </code>\
+    to receive random user data of the specified count.\
+    <br><br> Example for 5 users: <a href=https://uni-challenge-randuser.herokuapp.com/randusers/5>https://uni-challenge-randuser.herokuapp.com/randusers/5</a>"""
